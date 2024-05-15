e529212cb677f430a8aaf78914d5d1781543747a
narhari27 <narahari.eng@gmail.com>
2024-05-15 14:05:33 +0530

    added my assignment from mac

---
+++ basics-01-starting-project/assets/assignment/assign1.js
@@ -0,0 +1,8 @@
+let userInput = 10;
+let result
+result = 18 + userInput;
+result = result - 10;
+result = result * 2;
+result = result / 2;
+alert(result);
+alert(userInput);
---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,6 @@
+const defaultValue = 0;
+let currentResult = defaultValue;
+currentResult = (currentResult + 10)*2;
+let calculationDescription = `(' ${defaultValue}  + 10)*2`;
+
+outputResult(currentResult, calculationDescription)
