1850420a05892dd009339b5ae5da9b5c99c21d08
narhari27 <narahari.eng@gmail.com>
2024-05-17 16:16:52 +0530

    Added functions

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,13 @@
 const defaultValue = 0;
 let currentResult = defaultValue;
+
+function add(num1,num2) {
+    const result = num1 + num2;
+    alert('The result is ' + result);
+}
+add( 2,2);
+add( 1,5);
+
 currentResult = (currentResult + 10)*2;
 let calculationDescription = `(' ${defaultValue}  + 10)*2`;
 
--- basics-01-starting-project/basics-assignment-1-start/assignment.js
+++ basics-01-starting-project/basics-assignment-1-start/assignment.js
@@ -1,8 +1,8 @@
-let userInput = 10;
-let result
+let userInput = 20;
+let result;
 result = 18 + userInput;
 result = result - 10;
 result = result * 2;
-result = result / 2;
+result = result / 4;
 alert(result);
 alert(userInput);
