b7909788b798e298a430a3833396650d41753056
narhari27 <narahari.eng@gmail.com>
2024-07-04 11:54:03 +0530

    functions and operations completed

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,9 +1,56 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
-function add(num1,num2) {
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, '');    
+function getUserInput() {
+    return parseInt(usrInput.value);
 }
+function createAndWriteOutput(operator, resultBeforeCalc, CalcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${CalcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+function writeToLog(opertaionIdentifier, prevResult, operationNumber, newresult) {
+    const logEntry = {
+        operation: opertaionIdentifier,
+        prevResult: prevResult,
+        operand: operationNumber,
+        result: newresult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntry.operation);
+    console.log(logEntries);
+}
+function add() {
+    const enteredNumber = getUserInput();
+    const initalResult = currentResult;
+    console.log('input',enteredNumber , currentResult)
+    currentResult += enteredNumber;
+    createAndWriteOutput('+', initalResult, enteredNumber);
+    writeToLog('Add',initalResult,enteredNumber,currentResult);
 
+}
+function subtract() {
+    const enteredNumber = getUserInput();
+    const initalResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteOutput('-', initalResult, enteredNumber);
+    writeToLog('subtract',initalResult,enteredNumber,currentResult);
+}
+function multiply() {
+    const enteredNumber = getUserInput();
+    const initalResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteOutput('*', initalResult, enteredNumber);
+    writeToLog('multiply',initalResult,enteredNumber,currentResult);
+}
+function divide() {
+    const enteredNumber = getUserInput();
+    const initalResult = currentResult;
+    currentResult /= enteredNumber;
+    createAndWriteOutput('/', initalResult, enteredNumber);
+    writeToLog('divide',initalResult,enteredNumber,currentResult);
+}
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click ', multiply);
+divideBtn.addEventListener('click', divide);
--- basics-assignment-2-start/assignment.js
+++ basics-assignment-2-start/assignment.js
@@ -1,1 +1,20 @@
 const task3Element = document.getElementById('task-3');
+function greet(){
+    alert ('Hi There');
+}
+function greetPara (userName){
+alert('HI ' + userName);
+}
+function combine(str1 , str2 , str3){
+    const combineText = `${str1} ${str2} ${str3}` ;
+    return combineText;
+}
+greetPara('Narahari Gudagudi');
+
+task3Element.addEventListener('click' , greet);
+
+const combineString = combine('Hi', 'I am ' ,'Don');
+alert (combineString);
+
+
+
