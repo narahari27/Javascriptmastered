5e316b9e194b6e22dfeb22e7276d09045dc3079f
narahari gudagudi <narahari.gudagudi@ajrinfosystesms.com>
2024-07-17 10:38:56 +0530

    added healValue and bonusLife

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -6,8 +6,29 @@ const HEAL_VALUE=20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
+
+function endRound(){
+    let initialPlayerHealth = currentPlayerHealth; 
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -=playerDamage;
+    if(currentPlayerHealth<=0 && hasBonusLife){
+        hasBonusLife =false;
+        removeBonusLife();
+        currentPlayerHealth = initialPlayerHealth;
+        setPlayerHealth(initialPlayerHealth);
+        alert('Lucky you have a bonus life! so you are still alive fucker');
+    }
+    if (currentMonsterHealth<=0 && currentPlayerHealth > 0){
+        alert('you won ');
+    }else if (currentPlayerHealth<=0 && currentMonsterHealth > 0){
+        alert('you lost');
+    }else if(currentPlayerHealth<=0 && currentMonsterHealth <=0){
+        alert('its a Draw!!');
+    }
+}
 
 function attackMonster(mode){
     let maxDamage;
@@ -18,15 +39,7 @@ function attackMonster(mode){
     }
     const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
-    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-    currentPlayerHealth -=playerDamage;
-    if (currentMonsterHealth<=0 && currentPlayerHealth > 0){
-        alert('you won ');
-    }else if (currentPlayerHealth<=0 && currentMonsterHealth > 0){
-        alert('you lost');
-    }else if(currentPlayerHealth<=0 && currentMonsterHealth <=0){
-        alert('its a Draw!!');
-    }
+    endRound();
 }
 
 function attackHandler(){
@@ -37,7 +50,16 @@ function strongAttackHandler(){
     attackMonster('STRONG_ATTACK')
 }
 function healPlayerHandler(){
-increasePlayerHealth(HEAL_VALUE);
+let healValue;
+if(currentPlayerHealth>= chosenMaxLife - HEAL_VALUE){
+    alert("You cant add more health than initial health");
+    healValue = chosenMaxLife - currentPlayerHealth;
+}else{
+    healValue = HEAL_VALUE;
+}
+increasePlayerHealth(healValue);
+currentPlayerHealth +=healValue;
+endRound();
 }
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
